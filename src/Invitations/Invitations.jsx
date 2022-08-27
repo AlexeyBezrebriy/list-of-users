83b0ce2bae99793591da8a94237177e4f6007eac
Alexei <lohanecapho2000@gmail.com>
2022-08-27 17:32:21 +0300

    set up working directory

---
+++ src/Invitations/Invitations.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import Users from "../Users"
+
+function Invitations() {
+  return (
+    <div className="invitations__container">
+      <input type="text" name="" id="" />
+      <Users />
+      <button>Send invitation</button>
+    </div>
+  )
+}
+
+export default Invitations
